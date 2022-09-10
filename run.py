67100d84bb2d19d15176c3d19b80e2fed5049499
Agustin cilli <cilli.agustin@gmail.com>
2022-09-10 12:23:43 +0000

    Add funtion to style sudoku

--- run.py
+++ run.py
@@ -1,3 +1,45 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import colorama
+from colorama import Fore
+colorama.init()
+
+
+puzzle = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0]
+]
+
+
+def style_board(board):
+    """
+    Add style to the board to display correcly the sudoku
+    """
+    for x in range(len(board)):
+        if x == 0:
+            print(" - - - - - - - - - - - - - - - -")
+            print(f" |{Fore.CYAN} 1 2 3  {Fore.WHITE}|{Fore.CYAN} 4 5 6  {Fore.WHITE}|{Fore.CYAN} 7 8 9 {Fore.WHITE}|   |")
+            print(" - - - - - - - - - - - - - - - -")
+        elif x % 3 == 0:
+            print(" - - - - - - - - - - - - - - - -")
+        for y in range(len(board[0])):
+            currentVal = ""
+            if board[x][y] == 0:
+                currentVal = f"{Fore.RED}{str(board[x][y])}"
+            else:
+                currentVal = f"{Fore.GREEN}{str(board[x][y])}"
+            if y % 3 == 0:
+                print(f"{Fore.WHITE} | ", end="")
+            if y == 8:
+                print(currentVal + f"{Fore.WHITE} | {Fore.CYAN}{x + 1} {Fore.WHITE}|")
+            else:
+                print(currentVal + " ", end="")
+    print(" - - - - - - - - - - - - - - - -")
+
+
+style_board(puzzle)
