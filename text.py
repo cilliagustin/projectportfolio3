2c6c22e62e5ebf9192a3d4f4546deb0a2a75a20f
Agustin cilli <cilli.agustin@gmail.com>
2022-09-16 09:16:30 +0000

    join texts with titles and replace function to create example board for an example-data

--- text.py
+++ text.py
@@ -23,36 +23,28 @@ MAIN_MENU_TEXT = """
 Hello, and welcome to the Sudoku Solver App. Here you will be able finally to
 get the correct answer for the puzzle you have been trying all morning or just
 check if you complete it correctly.
-Write "start" to run the App, "information" to get more information about the
-game and how to use the App or "exit" to close the aplication.
 """
 
-INFORMATION_TITLE = """
+INFORMATION_TEXT = """
  _____ _   _ ______ ______________  ___  ___ _____ _____ _____ _   _ 
 |_   _| \ | ||  ___|  _  | ___ \  \/  | / _ \_   _|_   _|  _  | \ | |
   | | |  \| || |_  | | | | |_/ / .  . |/ /_\ \| |   | | | | | |  \| |
   | | | . ` ||  _| | | | |    /| |\/| ||  _  || |   | | | | | | . ` |
  _| |_| |\  || |   \ \_/ / |\ \| |  | || | | || |  _| |_\ \_/ / |\  |
  \___/\_| \_/\_|    \___/\_| \_\_|  |_/\_| |_/\_/  \___/ \___/\_| \_/
-"""
 
-INFORMATION_TEXT = """
 Do you wish to know more about the game Sudoku? Do you want to know how to use
-correctly the App? Write "Game" for more information on the Puzzle, "App" for
-a tutorial on the App, write "Back" to go to the Main Menu or "Exit" to close
-the App.
+correctly the App?
 """
 
-GAME_TITLE = """
+GAME_TEXT = """
  _____ _   _  _____   _____   ___  ___  ___ _____ 
 |_   _| | | ||  ___| |  __ \ / _ \ |  \/  ||  ___|
   | | | |_| || |__   | |  \// /_\ \| .  . || |__  
   | | |  _  ||  __|  | | __ |  _  || |\/| ||  __| 
   | | | | | || |___  | |_\ \| | | || |  | || |___ 
   \_/ \_| |_/\____/   \____/\_| |_/\_|  |_/\____/ 
-"""
 
-GAME_TEXT = """
 Although the name "Sudoku" comes from Japan, the earliest versions of this
 game came from France in the late 19th century, when some newspapers
 started experimenting with mathematical games that were displayed in 9x9
@@ -72,20 +64,17 @@ of the world under the "Sudoku" name.
 The rules are fairly simple, The user is given a 9x9 grid with certain amount
 of numbers already written (The less amount of numbers given the hardest is
 the game) and the goal of the player is to complete all the grid with numbers
-from 1 to 9 without repeating any number in a row, a
-column or a 3x3 square.
+from 1 to 9 without repeating any number in a row, a column or a 3x3 square.
 """
 
-APP_TITLE = """
+APP_TEXT = """
  _____ _   _  _____    ___  ____________ 
 |_   _| | | ||  ___|  / _ \ | ___ \ ___ |
   | | | |_| || |__   / /_\ \| |_/ / |_/ /
   | | |  _  ||  __|  |  _  ||  __/|  __/ 
   | | | | | || |___  | | | || |   | |    
-  \_/ \_| |_/\____/  \_| |_/\_|   \_|    
-"""
+  \_/ \_| |_/\____/  \_| |_/\_|   \_|   
 
-APP_TEXT = """
 To use the App is pretty simple. Once in the Main Menu write "Start", this
 will show you a 9x9 grid all completed by zeros, this number represents an
 unknown number. Use the guiding numbers colored in blue to locate a square
@@ -102,25 +91,17 @@ get the solution to the puzzle.
 """
 
 
-def print_example_board():
-    """
-    print an example board, used on information menu
-    """
-    print("- - - - - - - - - - - - - - - -")
-    print(f"|{Fore.CYAN} 1 2 3  {Fore.WHITE}|{Fore.CYAN} 4 5 6  {Fore.WHITE}|{Fore.CYAN} 7 8 9 {Fore.WHITE}|   |")
-    print("- - - - - - - - - - - - - - - -")
-    print(f"|{Fore.RED} 0 0 0  {Fore.WHITE}|{Fore.RED} 0 0 0  {Fore.WHITE}|{Fore.RED} 0 0 0 {Fore.WHITE}|{Fore.CYAN} 1 {Fore.WHITE}|")
-    print(f"|{Fore.RED} 0 0 0  {Fore.WHITE}|{Fore.RED} 0 0 0  {Fore.WHITE}|{Fore.RED} 0 0 0 {Fore.WHITE}|{Fore.CYAN} 2 {Fore.WHITE}|")
-    print(f"|{Fore.RED} 0 0 0  {Fore.WHITE}|{Fore.RED} 0 0 0  {Fore.WHITE}|{Fore.RED} 0 {Fore.GREEN}4 {Fore.RED}0 {Fore.WHITE}|{Fore.CYAN} 3 {Fore.WHITE}|")
-    print("- - - - - - - - - - - - - - - -")
-    print(f"|{Fore.RED} 0 {Fore.GREEN}9 {Fore.RED}0  {Fore.WHITE}|{Fore.RED} 0 0 0  {Fore.WHITE}|{Fore.RED} 0 0 0 {Fore.WHITE}|{Fore.CYAN} 4 {Fore.WHITE}|")
-    print(f"|{Fore.RED} 0 0 0  {Fore.WHITE}|{Fore.RED} 0 0 0  {Fore.WHITE}|{Fore.RED} 0 0 0 {Fore.WHITE}|{Fore.CYAN} 5 {Fore.WHITE}|")
-    print(f"|{Fore.RED} 0 0 0  {Fore.WHITE}|{Fore.RED} 0 0 0  {Fore.WHITE}|{Fore.RED} 0 0 0 {Fore.WHITE}|{Fore.CYAN} 6 {Fore.WHITE}|")
-    print("- - - - - - - - - - - - - - - -")
-    print(f"|{Fore.RED} 0 0 {Fore.GREEN}5  {Fore.WHITE}|{Fore.RED} 0 0 0  {Fore.WHITE}|{Fore.RED} 0 0 0 {Fore.WHITE}|{Fore.CYAN} 7 {Fore.WHITE}|")
-    print(f"|{Fore.RED} 0 0 0  {Fore.WHITE}|{Fore.RED} 0 0 0  {Fore.WHITE}|{Fore.RED} 0 0 0 {Fore.WHITE}|{Fore.CYAN} 8 {Fore.WHITE}|")
-    print(f"|{Fore.RED} 0 0 0  {Fore.WHITE}|{Fore.RED} 0 0 0  {Fore.WHITE}|{Fore.RED} 0 0 0 {Fore.WHITE}|{Fore.CYAN} 9 {Fore.WHITE}|")
-    print("- - - - - - - - - - - - - - - -")
+EXAMPLE_BOARD = [
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 4, 0],
+  [0, 9, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 5, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0]
+]
 
 
 APP_TEXT2 = """
