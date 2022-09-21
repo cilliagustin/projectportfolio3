2c4df45c9ec1882494e5483214106d9c159803c5
Agustin cilli <cilli.agustin@gmail.com>
2022-09-21 17:26:32 +0000

    replace doctring for f string to use colorama in some texts of text.py

--- text.py
+++ text.py
@@ -67,28 +67,35 @@ the game) and the goal of the player is to complete all the grid with numbers
 from 1 to 9 without repeating any number in a row, a column or a 3x3 square.
 """
 
-APP_TEXT = """
+APP_TITLE = """
  _____ _   _  _____    ___  ____________ 
 |_   _| | | ||  ___|  / _ \ | ___ \ ___ |
   | | | |_| || |__   / /_\ \| |_/ / |_/ /
   | | |  _  ||  __|  |  _  ||  __/|  __/ 
   | | | | | || |___  | | | || |   | |    
   \_/ \_| |_/\____/  \_| |_/\_|   \_|   
-
-To use the App is pretty simple. Once in the Main Menu write "Start", this
-will show you a 9x9 grid all completed by zeros, this number represents an
-unknown number. Use the guiding numbers colored in blue to locate a square
-where you have a number that was given by you in the game and insert it.
-The way you do this is by entering 3 numbers separated by a comma, a
-hyphen, or a space. The first one being the row position, the second being
-the column position and the third one the value.
-This means that "4,2,9" would be that in the fourth row and second column
-is a nine, entering "7-3-5" would add in the seventh row and third column
-a five, and writing "3 8 4" would add in the third row and eighth column
-a four.
-Once you write all the numbers given by you by the game, write solve to
-get the solution to the puzzle.
 """
+
+APP_TEXT = (
+    'To use the App is pretty simple. Once in the Main Menu write '
+    f'{Fore.BLUE}start{Fore.WHITE}, this will show you a 9x9 grid all '
+    'completed by zeros, this number represents an unknown number. Use the  '
+    f'guiding numbers colored in {Fore.BLUE}blue{Fore.WHITE} to locate a '
+    'square where you have a number that was given by you in the game and '
+    f'insert it. The way you do this is by entering 3 numbers separated by a '
+    f'{Fore.BLUE}comma{Fore.WHITE}, a {Fore.BLUE}hyphen{Fore.WHITE}, or a '
+    f'{Fore.BLUE}space{Fore.WHITE}. The first one being the {Fore.BLUE}row '
+    f'{Fore.WHITE}position, the second being the {Fore.BLUE}column '
+    f'{Fore.WHITE}position and the third one the {Fore.BLUE}value{Fore.WHITE}.'
+    f'\nThis means that {Fore.BLUE}4,2,9{Fore.WHITE} would be that in the '
+    f'fourth{Fore.BLUE} row{Fore.WHITE} and second {Fore.BLUE}column'
+    f'{Fore.WHITE} is a {Fore.BLUE}nine{Fore.WHITE}, entering {Fore.BLUE}7-3-5'
+    f'{Fore.WHITE} would add in the seventh {Fore.BLUE}row{Fore.WHITE} and '
+    f'third {Fore.BLUE}column{Fore.WHITE} a {Fore.BLUE}five{Fore.WHITE}, and '
+    f'writing {Fore.BLUE}3 8 4{Fore.WHITE} would add in the third {Fore.BLUE}'
+    f'row {Fore.WHITE} and eighth {Fore.BLUE}column{Fore.WHITE} a {Fore.BLUE}'
+    f'four {Fore.WHITE}Once you write all the numbers given by you by the game'
+    f', write {Fore.BLUE}solve{Fore.WHITE} to get the solution to the puzzle.')
 
 
 EXAMPLE_BOARD = [
@@ -104,19 +111,22 @@ EXAMPLE_BOARD = [
 ]
 
 
-APP_TEXT2 = """
-If you made a mistake, and you misplaced a given number you can always replace
-that position with an unknown number by entering 0 in the third value:
-Entering "7,3,0", "7-3-0" or "7 3 0" would make again the seventh row and
-third column an unknown number.
-You can also write "Reset" to delete all values on the puzzle, starting all
-over again.
-"""
+APP_TEXT2 = (
+    f'If you made a {Fore.RED}mistake{Fore.WHITE}, and you misplaced a given '
+    'number you can always replace that position with an unknown number by '
+    f'entering {Fore.BLUE}0{Fore.WHITE} in the {Fore.BLUE}third{Fore.WHITE} '
+    f'value: Entering {Fore.BLUE}7,3,0{Fore.WHITE}, {Fore.BLUE}7-3-0'
+    f'{Fore.WHITE} or {Fore.BLUE}7 3 0{Fore.WHITE} would make again the '
+    f'seventh {Fore.BLUE}row{Fore.WHITE} and third {Fore.BLUE}column '
+    f'{Fore.WHITE}an unknown number.\n You can also write {Fore.BLUE}reset'
+    f'{Fore.WHITE} to delete all values on the puzzle, starting all over '
+    'again.')
 
-START_APP_TEXT = """
-Write all your given numbers using one of the following paramethers:
-"2,9,4", "2-9-4" or "2 9 4". Remember the first number represents the row,
-the second the column and the third the given number. Remember the number 0
-represents an unknown number.
-Your puzzle so far: 
-"""
+START_APP_TEXT = (
+    'Write all your given numbers using one of the following paramethers: ba'
+    f'{Fore.BLUE}2,9,4{Fore.WHITE}, {Fore.BLUE}2-9-4{Fore.WHITE} or{Fore.BLUE}'
+    f' 2 9 4{Fore.WHITE}. Remember the first number represents the {Fore.BLUE}'
+    f'row {Fore.WHITE}, the second the {Fore.BLUE}column{Fore.WHITE} and the '
+    f'third the {Fore.BLUE}given number{Fore.WHITE}.\nAlso remember the number'
+    f'{Fore.BLUE} 0{Fore.WHITE} represents an unknown number.\n Your puzzle so'
+    ' far:')
