69b0c18d29aa5bc187b5c793228b8d9839ff98af
Agustin cilli <cilli.agustin@gmail.com>
2022-10-05 08:18:07 +0000

    Edit wrong spacing in texts in text.py

--- text.py
+++ text.py
@@ -36,7 +36,7 @@ INFORMATION_TEXT = (
   "\n \\___/\\_| \\_/\\_|    \\___/\\_| \\_\\_|  |_/\\_| |_/\\_/  \\___/ \\__"
   "_/\\_| \\_/"
   "\n\nDo you wish to know more about the game Sudoku? Do you want to know how"
-  " to use correctly the App?"
+  " to use\ncorrectly the App?"
 )
 
 GAME_TEXT = """
@@ -80,24 +80,24 @@ APP_TITLE = """
 
 APP_TEXT = (
     'To use the App is pretty simple. Once in the Main Menu write '
-    f'{Fore.BLUE}start{Fore.WHITE}, this will show you a 9x9 grid all '
-    'completed by zeros, this number represents an unknown number. Use the  '
+    f'{Fore.BLUE}start{Fore.WHITE}, this will\nshow you a 9x9 grid all '
+    'completed by zeros, this number represents an unknown\nnumber. Use the '
     f'guiding numbers colored in {Fore.BLUE}blue{Fore.WHITE} to locate a '
-    'square where you have a number that was given by you in the game and '
-    f'insert it. The way you do this is by entering 3 numbers separated by a '
+    'square where you \nhave a number that was given by you in the game and '
+    f'insert it. The way you do\nthis is by entering 3 numbers separated by a '
     f'{Fore.BLUE}comma{Fore.WHITE}, a {Fore.BLUE}hyphen{Fore.WHITE}, a '
-    f'{Fore.BLUE}space{Fore.WHITE} or just write them all together with '
+    f'{Fore.BLUE}space{Fore.WHITE} or just\nwrite them all together with '
     f'{Fore.BLUE}no separation{Fore.WHITE} at all. \nThe first one being the '
     f'{Fore.BLUE}row {Fore.WHITE}position, the second being the {Fore.BLUE}'
-    f'column {Fore.WHITE}position and the third one the {Fore.BLUE}value'
+    f'column {Fore.WHITE}position and\nthe third one the {Fore.BLUE}value'
     f'{Fore.WHITE}.\nThis means that {Fore.BLUE}4,2,9{Fore.WHITE} would be '
     f'that in the fourth{Fore.BLUE} row{Fore.WHITE} and second {Fore.BLUE}'
-    f'column{Fore.WHITE} is a {Fore.BLUE}nine{Fore.WHITE}, entering '
+    f'column{Fore.WHITE} is a {Fore.BLUE}\nnine{Fore.WHITE}, entering '
     f'{Fore.BLUE}7-3-5{Fore.WHITE} would add in the seventh {Fore.BLUE}row'
     f'{Fore.WHITE} and third {Fore.BLUE}column{Fore.WHITE} a {Fore.BLUE}five'
-    f'{Fore.WHITE}, writing {Fore.BLUE}3 8 4{Fore.WHITE} would add in the '
+    f'{Fore.WHITE},\nwriting {Fore.BLUE}3 8 4{Fore.WHITE} would add in the '
     f'third {Fore.BLUE}row {Fore.WHITE} and eighth {Fore.BLUE}column'
-    f'{Fore.WHITE} a {Fore.BLUE}four{Fore.WHITE}, and adding {Fore.BLUE}872'
+    f'{Fore.WHITE} a {Fore.BLUE}four{Fore.WHITE}, and adding{Fore.BLUE}\n872'
     f'{Fore.WHITE} would add in the eighth {Fore.BLUE}row {Fore.WHITE} and the'
     f' seventh {Fore.BLUE}column{Fore.WHITE} a {Fore.BLUE}two{Fore.WHITE}.'
     f'\nOnce you write all the numbers given by you by the game, write '
@@ -119,34 +119,34 @@ EXAMPLE_BOARD = [
 
 APP_TEXT2 = (
     f'If you made a {Fore.RED}mistake{Fore.WHITE}, and you misplaced a given '
-    'number you can always replace that position with an unknown number by '
+    'number you can always replace\nthat position with an unknown number by '
     f'entering {Fore.BLUE}0{Fore.WHITE} in the {Fore.BLUE}third{Fore.WHITE} '
     f'value: Entering {Fore.BLUE}7,3,0{Fore.WHITE}, {Fore.BLUE}7-3-0'
     f'{Fore.WHITE}, {Fore.BLUE}7 3 0{Fore.WHITE} or {Fore.BLUE}730{Fore.WHITE}'
     f' would make again the seventh {Fore.BLUE}row{Fore.WHITE} and third '
-    f'{Fore.BLUE}column {Fore.WHITE}an unknown number.\n You can also write '
+    f'{Fore.BLUE}column {Fore.WHITE}an unknown number.\nYou can also write '
     f'{Fore.BLUE}reset{Fore.WHITE} to delete all values on the puzzle, '
-    'starting all over again.\n\n')
+    'starting all over\nagain.\n\n')
 
 START_APP_TEXT = (
     'Write all your given numbers using one of the following paramethers: '
-    f'{Fore.BLUE}2,9,4{Fore.WHITE}, {Fore.BLUE}2-9-4{Fore.WHITE},{Fore.BLUE}'
+    f'{Fore.BLUE}2,9,4{Fore.WHITE},\n{Fore.BLUE}2-9-4{Fore.WHITE},{Fore.BLUE}'
     f' 2 9 4{Fore.WHITE} or {Fore.BLUE}294{Fore.WHITE}. Remember the first '
     f'number represents the {Fore.BLUE} row {Fore.WHITE}, the second the '
     f'{Fore.BLUE}column{Fore.WHITE} and the third the {Fore.BLUE}given number'
     f'{Fore.WHITE}.\nAlso remember the number{Fore.BLUE} 0{Fore.WHITE} '
-    'represents an unknown number.\n Your puzzle so far:')
+    'represents an unknown number.\nYour puzzle so far:')
 
 VALID_FORMAT = (
   f'\nRemember all your given values must be entered all together{Fore.BLUE} '
-  f'without spaces{Fore.WHITE} or separated by either {Fore.BLUE}commas'
+  f'without spaces{Fore.WHITE} or\nseparated by either {Fore.BLUE}commas'
   f'{Fore.WHITE}, {Fore.BLUE}hyphens{Fore.WHITE} or {Fore.BLUE}spaces'
   f'{Fore.WHITE}.\nThe {Fore.BLUE}first{Fore.WHITE} number represents the '
   f'{Fore.BLUE}row{Fore.WHITE}, the {Fore.BLUE}second{Fore.WHITE} the '
   f'{Fore.BLUE}column{Fore.WHITE} and the {Fore.BLUE}third{Fore.WHITE} the '
-  f'{Fore.BLUE}value{Fore.WHITE} ({Fore.BLUE}zero{Fore.WHITE} being an '
+  f'{Fore.BLUE}\nvalue{Fore.WHITE} ({Fore.BLUE}zero{Fore.WHITE} being an '
   f'{Fore.BLUE}unknown number{Fore.WHITE}) e.g.:\n{Fore.BLUE}4 8 2'
   f'{Fore.WHITE}, {Fore.BLUE}4-8-2{Fore.WHITE}, {Fore.BLUE}4,8,2{Fore.WHITE} '
   f'or {Fore.BLUE}482{Fore.WHITE} would be equal to add in the {Fore.BLUE}'
-  f'fourth row{Fore.WHITE} and{Fore.BLUE} eighth column {Fore.WHITE}a '
+  f'fourth row{Fore.WHITE} and{Fore.BLUE} eighth\ncolumn {Fore.WHITE}a '
   f'{Fore.BLUE}2{Fore.WHITE}.\n')
